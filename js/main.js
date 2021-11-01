c8379c2a9fc32d49813a4c6f0f259e1bad69e604
unknown <shisonokyojin@icloud.com>
2021-11-01 13:54:27 +0200

    add new question form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+class Question {
+  constructor(
+    question,
+    answer,
+    value,
+    created_at,
+    category
+  ) {
+    this.question = question
+    this.answer = answer
+    this.value = value
+    this.created_at = created_at
+    this.category = category
+  }
+}
+
+function closeDialog() {
+  $("#modal").get(0).style.display = "none";
+  resetForm()
+}
+
+function resetForm() {
+  $("#form").get(0).reset();
+}
+
+function showDialog() {
+  $("#modal").get(0).style.display = "block";
+}
 
 function getQuestions() {
   /*var temp = document.getElementsByTagName("template")[0];
@@ -30,7 +58,15 @@ function downloadQuestions() {
         let qs = JSON.parse(this.response);
         for(let x in qs) {
           let question = qs[x]
-          insertQuestion(question)
+          insertQuestion(
+            new Question(
+              question.question,
+              question.answer,
+              question.value,
+              question.created_at,
+              question.category.title
+            )
+          )
         }
       }
       hideSpinner()
@@ -46,17 +82,17 @@ function downloadQuestions() {
 
 function insertQuestion(question) {
   let clone = $("#question_template").clone().get()["0"].content
-
   let text = clone.querySelector("#question")
   let answer = clone.querySelector("#answer")
   let value = clone.querySelector("#value")
   let created_at = clone.querySelector("#created_at")
   let title = clone.querySelector("#title")
+
   text.textContent = question.question
   answer.textContent = question.answer
   value.textContent = question.value
   created_at.textContent = question.created_at
-  title.textContent = question.category.title
+  title.textContent = question.category
 
   var new_item = $("<div></div>").append(clone).hide();
   //var new_item = $('<b>hello</b><br><b>hello2</b>').hide();
@@ -64,4 +100,69 @@ function insertQuestion(question) {
   new_item.slideDown('slow')
   //$(response).hide().fadeIn('slow')
   //$(clone).hide().prependTo($("#questions")).hide().fadeIn(1000)
+}
+
+function validateInput(input, errorIcon, checkIcon, errorMessage, isValid, errorMessageValue) {
+  if(isValid(input.value)) {
+    input.style.border = "2px solid green"
+    errorIcon.style.opacity = "0"
+    checkIcon.style.opacity = "1"
+    errorMessage.innerHTML = ""
+    return true
+  } else {
+    input.style.border = "2px solid red"
+    errorIcon.style.opacity = "1"
+    checkIcon.style.opacity = "0"
+    errorMessage.innerHTML = errorMessageValue
+    return false
+  }
+}
+
+
+function onSubmit() {
+  let question = document.getElementById("f-question")
+  let answer = document.getElementById("f-answer")
+  let value = document.getElementById("f-value")
+  let created_at = document.getElementById("f-created-at")
+  let category = document.getElementById("f-category")
+
+  let question_error_icon = document.getElementById("f-question-error-icon")
+  let answer_error_icon = document.getElementById("f-answer-error-icon")
+  let value_error_icon = document.getElementById("f-value-error-icon")
+  let created_at_error_icon = document.getElementById("f-created-at-error-icon")
+  let category_error_icon = document.getElementById("f-category-error-icon")
+
+  let question_error_message = document.getElementById("f-question-error-message")
+  let answer_error_message = document.getElementById("f-answer-error-message")
+  let value_error_message = document.getElementById("f-value-error-message")
+  let created_at_error_message = document.getElementById("f-created-at-error-message")
+  let category_error_message = document.getElementById("f-category-error-message")
+
+  let question_check_icon = document.getElementById("f-question-check-icon")
+  let answer_check_icon = document.getElementById("f-answer-check-icon")
+  let value_check_icon = document.getElementById("f-value-check-icon")
+  let created_at_check_icon = document.getElementById("f-created-at-check-icon")
+  let category_check_icon = document.getElementById("f-category-check-icon")
+
+  let a = validateInput(question, question_error_icon, question_check_icon, question_error_message, (str => str.length >= 10), "Must be at least 10 characters length")
+  let b = validateInput(answer, answer_error_icon, answer_check_icon, answer_error_message, (str => str.length >= 5), "Must be at least 10 characters length")
+  let c = validateInput(value, value_error_icon, value_check_icon, value_error_message, (str => {let x = parseInt(str); return (x >= 50 && x <= 150)}), "Must be between 50 and 150")
+  let d = validateInput(created_at, created_at_error_icon, created_at_check_icon, created_at_error_message, (str => str.length > 0), "Cannot be empty")
+  let e = validateInput(category, category_error_icon, category_error_message, category_check_icon, (str => {return true}), "")
+
+  if(a && b && c && d && e) {
+    let q = new Question(
+      question.value,
+      answer.value,
+      value.value,
+      created_at.value,
+      category.value
+    )
+    insertQuestion(q)
+    closeDialog()
+    let form = document.getElementById("form")
+    form.reset()
+  }
+
+  return false
 }
